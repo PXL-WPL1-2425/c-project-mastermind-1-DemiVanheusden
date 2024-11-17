054cf27c3668a80d16830fd2260a07ed60c679ff
DemiVanheusden <demi.vanheusden@student.pxl.be>
2024-11-17 17:47:43 +0100

    Mastermind 02- Code

--- MasterMind/MasterMind/MainWindow.xaml.cs
+++ MasterMind/MasterMind/MainWindow.xaml.cs
@@ -16,11 +16,27 @@ namespace MasterMind
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
+
+        SolidColorBrush[] colorSelection = [Brushes.Red, Brushes.Blue, Brushes.Green, Brushes.White, Brushes.Yellow, Brushes.Orange];
+        string[] colorSelectionString = ["Red", "Blue", "Green", "White", "Yellow", "Orange"];
+        int[] colorsRandom = new int[4];
+
         public MainWindow()
         {
             InitializeComponent();
+            pickColors();
         }
 
+        private void pickColors()
+        {
+            for (int i = 0; i < colorsRandom.Length; i++)
+            {
+                colorsRandom[i] = rnd.Next(1, 6);
+            }
+
+            this.Title = $"Mastermind ({colorSelectionString[colorsRandom[0]]}, {colorSelectionString[colorsRandom[1]]}, {colorSelectionString[colorsRandom[2]]}, {colorSelectionString[colorsRandom[3]]})";
+        }
 
         private void checkcode_Click(object sender, RoutedEventArgs e)
         {
